4ae32d1e7bd395a8137025df98e68a4be5c43d07
Ryan Tate <ryan@ventureequity.co>
2016-07-20 09:48:27 -0700

    Initial commit of Poloniex Trading Bot

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  currencyPairs : [
+    "BTC_ETH",
+    "USDT_BTC",
+    "USDT_ETH",
+  ],
+  start : 1435699200, // 06/30/2015
+  end : 9999999999,
+  period : 7200, // Every 2 hours
+  dataDir : `${__dirname}/data`,
+};
---
+++ index.js
@@ -0,0 +1,13 @@
+import Analytics from './lib/analytics';
+import ChartData from './lib/chartData';
+import TickerFeed from './lib/tickerFeed';
+import Statistics from './lib/statistics';
+import SMA from './lib/indicators/sma';
+
+export {
+  Analytics,
+  ChartData,
+  TickerFeed,
+  SMA,
+  Statistics,
+}
